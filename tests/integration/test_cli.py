54eaac4cb8a2afb2850045528ec1411b791b9b4d
Nicholas Sun <olassun2@gmail.com>
2017-03-21 20:00:56 -0700

    tests: Refactor to use proper ping/pong in tests
    Previously just checked for a 200 ok. Also added tests for SRV.

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -1,3 +1,4 @@
+import json
 import os
 import shlex
 import signal
@@ -5,6 +6,7 @@ import subprocess
 import sys
 import time
 
+from dcos import util
 from dcos_tunnel import cli
 
 from common import ssh_output
@@ -14,20 +16,91 @@ Host {host}
     Port 22
 """
 
+TEST_APP_NAME = "dcos-tunnel-cli-integration-app"
+TEST_PORT_NAME = "test"
+TEST_PORT_NUMBER = 80
+TEST_PORT_PROTOCOL = "tcp"
+
+TEST_SRV = "_{}._{}._{}.marathon.mesos".format(TEST_PORT_NAME,
+                                               TEST_APP_NAME,
+                                               TEST_PORT_PROTOCOL)
+TEST_ADDR = "{}.marathon.mesos".format(TEST_APP_NAME)
+
+TRANSPARENT_SUFFIX = ".mydcos.directory"
+
+STANDARD_MODE = "standard"
+TRANSPARENT_MODE = "transparent"
+
+
+def setup_module():
+    print("SETUP")
+
+    app = {
+        'id': "/{}".format(TEST_APP_NAME),
+        'cpus': 0.1,
+        'mem': 32,
+        'instances': 1,
+        'cmd': '/opt/mesosphere/bin/dcos-shell python '
+               '/opt/mesosphere/active/dcos-integration-test/util/python_test_server.py $PORT0',
+        'env': {
+            'DCOS_TEST_UUID': TEST_APP_NAME,
+            # required for python_test_server.py to run as nobody
+            'HOME': '/'
+        },
+        'healthChecks': [
+            {
+                'protocol': 'MESOS_HTTP',
+                'path': '/ping',
+                'portIndex': 0,
+                'gracePeriodSeconds': 5,
+                'intervalSeconds': 10,
+                'timeoutSeconds': 10,
+                'maxConsecutiveFailures': 3
+            }
+        ],
+        'portDefinitions': [{
+            "protocol": TEST_PORT_PROTOCOL,
+            "port": TEST_PORT_NUMBER,
+            "name": TEST_PORT_NAME
+        }],
+        'acceptedResourceRoles': ["slave_public"],
+        'requirePorts': True,
+    }
+
+    with util.temptext() as file_tup:
+        f, fpath = file_tup
+        app_bits = json.dumps(app).encode()
+        os.write(f, app_bits)
+        os.fsync(f)
+        add_com = "dcos marathon app add {}".format(fpath)
+        subprocess.call(shlex.split(add_com))
+
+        time.sleep(5)
+        for _ in range(360):
+            ret = subprocess.call(shlex.split("dcos marathon deployment list"))
+            if ret != 0:
+                break
+            time.sleep(1)
+
+
+def teardown_module():
+    print("TEARDOWN")
+    subprocess.call(shlex.split("dcos marathon app remove {}".format(TEST_APP_NAME)))
+
 
 def test_empty_config():
     local_port = "8800"
     config_file = "/tmp/dcos_tunnel_test_empty_config"
     targs = ["--port", local_port, "--config-file", config_file]
-    args = ("curl -sS -v --proxy http://127.0.0.1:8800 " +
-            "--fail marathon.mesos.mydcos.directory")
+    url = "{}/ping".format(TEST_ADDR)
+    cmd = "curl -sS -v --proxy http://127.0.0.1:8800 --fail {}".format(url)
 
     f = open(config_file, 'w+')
     f.write("")
     f.close()
 
-    _, _, ret, tret = _tunnel_runner("http", targs, args)
-    assert ret == 0
+    success, tret = _tunnel_runner("http", targs, cmd)
+    assert success is True
     assert _tunnel_success(tret)
 
 
@@ -35,25 +108,25 @@ def test_simple_config():
     local_port = "8800"
     config_file = "/tmp/dcos_tunnel_test_empty_config"
     targs = ["--port", local_port, "--config-file", config_file]
-    args = ("curl -sS -v --proxy http://127.0.0.1:8800 " +
-            "--fail marathon.mesos.mydcos.directory")
+    url = "{}/ping".format(TEST_ADDR)
+    cmd = "curl -sS -v --proxy http://127.0.0.1:8800 --fail {}".format(url)
     host = cli.get_host("")
 
     f = open(config_file, 'w+')
     f.write(ssh_config_fmt.format(host=host))
     f.close()
 
-    _, _, ret, tret = _tunnel_runner("http", targs, args)
-    assert ret == 0
+    success, tret = _tunnel_runner("http", targs, cmd)
+    assert success is True
     assert _tunnel_success(tret)
 
 
 def test_socks_lookup():
     targs = ["--option StrictHostKeyChecking=no", "--port", "1080"]
-    args = ("curl -sS -v --proxy socks5h://127.0.0.1:1080 " +
-            "--fail marathon.mesos.mydcos.directory")
-    _, _, ret, tret = _tunnel_runner("socks", targs, args)
-    assert ret == 0
+    url = "{}/ping".format(TEST_ADDR)
+    cmd = "curl -sS -v --proxy socks5h://127.0.0.1:1080 --fail {}".format(url)
+    success, tret = _tunnel_runner("socks", targs, cmd)
+    assert success is True
     assert _tunnel_success(tret)
 
 
@@ -66,31 +139,57 @@ def test_vpn_lookup():
 
     # XXX Hard-coded IP address of a DNS host from within a DC/OS cluster
     dns_host = '198.51.100.1'
-    args = ("ping -c1 $(host -t A master.mesos {} | ".format(dns_host) +
-            "tail -n1 | rev | cut -f1 -d' ' | rev)")
 
-    _, _, ret, tret = _tunnel_runner("vpn", targs, args)
-    assert ret == 0
+    ip = '"$(dig @{} +short {})"'.format(dns_host, TEST_ADDR)
+    url = "{}/ping".format(ip)
+    cmd = "curl -sS -v --fail {}".format(url)
+
+    success, tret = _tunnel_runner("vpn", targs, cmd)
+    assert success is True
     assert _tunnel_success(tret)
     assert not _dangling_proc(['openvpn --config'])
 
 
 def test_http_lookup():
+    _http_lookup_helper(TEST_ADDR)
+
+
+def test_http_srv_lookup():
+    _http_lookup_helper(TEST_SRV)
+
+
+def _http_lookup_helper(addr):
     local_port = "8800"
     targs = ["--port", local_port]
-    args = ("curl -sS -v --proxy http://127.0.0.1:8800 " +
-            "--fail marathon.mesos.mydcos.directory")
-    _, _, ret, tret = _tunnel_runner("http", targs, args)
-    assert ret == 0
+    url = "{}/ping".format(addr)
+    cmd = "curl -sS -v --proxy http://127.0.0.1:8800 --fail {}".format(url)
+    success, tret = _tunnel_runner("http", targs, cmd)
+    assert success is True
     assert _tunnel_success(tret)
 
 
 def test_http_transparent_lookup():
+    _http_transparent_lookup_helper(TEST_ADDR + TRANSPARENT_SUFFIX)
+
+
+def test_http_srv_transparent_lookup():
+    srv = TEST_SRV + TRANSPARENT_SUFFIX
+
+    # For some reason, on linux cURL is failing host resolution instead
+    # of resolving *.mydcos.directory to localhost. So we manually do
+    # a resolve before passing to cURL.
+    curl_flags = '--resolve "{srv}:80:$(dig +short {srv})"'.format(srv=srv)
+
+    _http_transparent_lookup_helper(srv, curl_flags=curl_flags)
+
+
+def _http_transparent_lookup_helper(addr, curl_flags=""):
     local_port = "80"
     targs = ["--port", local_port]
-    args = "curl -sS -v --fail marathon.mesos.mydcos.directory"
-    _, _, ret, tret = _tunnel_runner("http", targs, args)
-    assert ret == 0
+    url = "{}/ping".format(addr)
+    cmd = "curl -sS -v --fail {} {}".format(curl_flags, url)
+    success, tret = _tunnel_runner("http", targs, cmd)
+    assert success is True
     assert _tunnel_success(tret)
 
 
@@ -108,12 +207,18 @@ def _dangling_proc(procstrlist):
     return False
 
 
-def _tunnel_runner(ttype, tunnel_args, args, delay=15):
+def _tunnel_runner(ttype, tunnel_args, args, delay=30):
     tunnel = _tunnel(ttype, tunnel_args)
+    success = False
     endtime = time.time() + delay
     while time.time() < endtime:
         stdout, stderr, ret = ssh_output(args)
-        if ret == 0:
+        stdout_str = stdout.decode()
+        stderr_str = stderr.decode()
+        print("tunnel runner query stdout: {}".format(stdout_str))
+        print("tunnel runner query stderr: {}".format(stderr_str))
+        if ret == 0 and json.loads(stdout.decode()) == {"pong": True}:
+            success = True
             break
 
     if sys.platform == 'win32':
@@ -128,7 +233,7 @@ def _tunnel_runner(ttype, tunnel_args, args, delay=15):
     print('Runner STDOUT: {}'.format(tout.decode()))
     print('Runner STDERR: {}'.format(terr.decode()))
 
-    return (stdout, stderr, ret, tunnel.returncode)
+    return (success, tunnel.returncode)
 
 
 def _tunnel_success(tunnel_returncode):
